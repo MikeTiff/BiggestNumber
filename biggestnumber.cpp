1023fd7d3f518b82b81bdd5045ec1ffca3d97dc8
MikeTiff <mtiffany@mail.csuchico.edu>
2018-02-05 17:10:59 -0800

    Setup For inputs

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,11 +5,12 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  
+  int FirstNumber;
+  int SecNumber;
   cout<<"Please enter a whole number:\n";
-  
+  cin>> FirstNumber;
   cout<<"Please enter another whole number:\n";
-
+  cin>> SecNumber;
   cout<<"Of those two numbers, the biggest is: ";
   cout<<endl<<"Thank you for playing.\n";
 
