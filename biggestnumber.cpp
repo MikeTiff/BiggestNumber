1aa9f9858c3347c61ce7fc338c51686538397f75
MikeTiff <mtiffany@mail.csuchico.edu>
2018-02-05 17:25:36 -0800

    Fully Running Higher Number Thing

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -5,14 +5,19 @@ using namespace std; //so we don't need the prefix std::
 
 int main()
 {
-  int FirstNumber;
-  int SecNumber;
-  cout<<"Please enter a whole number:\n";
-  cin>> FirstNumber;
-  cout<<"Please enter another whole number:\n";
-  cin>> SecNumber;
-  cout<<"Of those two numbers, the biggest is: ";
-  cout<<endl<<"Thank you for playing.\n";
+        int FirstNumber = 0;
+        int SecNumber = 0;
+        int A = 0;
+        cout<<"Please enter a whole number:\n";
+        cin>> FirstNumber;
+        cout<<"Please enter another whole number:\n";
+        cin>> SecNumber;
+        if(FirstNumber > SecNumber)
+        {A = FirstNumber;}
+        else
+        {A = SecNumber;}
+        cout<<"Of those two numbers, the biggest is:"<<A;
+        cout<<endl<<"Thank you for playing.\n";
 
-  return 0;
+        return 0;
 }
